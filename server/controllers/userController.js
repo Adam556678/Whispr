8e9d4d7b26435d790ba08b213f36c8ac828b8942
Adam556678 <mahmudramadan75@yahoo.com>
2025-10-23 12:47:55 +0300

    Adding user authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,13 +4,65 @@ const jwt = require("jsonwebtoken");
 const validator = require("validator");
 const UserModel = require("../models/UserModel.js");
 
+const createToken = (_id) => {
+    const jwtkey = process.env.JWT_SECRET;
+
+    return jwt.sign({_id}, jwtkey, {expiresIn: "3d"});
+};
+
 const registerUser = async (req, res) => {
     const {name, email, password} = req.body;
 
-    const user = await UserModel.findOne({email});
-    if (user) return res.status(400).json({message: "User with the given email already exists"});
-
-    if (!email || !password || !name) return res.status(400).json({message: "All fields are required"});
+    try {
+        let user = await UserModel.findOne({email});
+        if (user) return res.status(400).json({message: "User with the given email already exists"});
+    
+        if (!email || !password || !name) 
+            return res.status(400).json({message: "All fields are required"});
+    
+        if (!validator.isEmail(email)) 
+            return res.status(400).json({message: "Email must be a valid email"});
+        
+        if (!validator.isStrongPassword(password))
+            return res.status(400).json({message: "Password must be a strong password"});
+    
+        user = new UserModel({name, email, password});
+    
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+    
+        await user.save();
+    
+        const token = createToken(user._id);
+        
+        res.status(201).json({message: "User is registered", data: {
+            _id: user._id,
+            name,
+            email,
+            token
+        }});
+    } catch (error) {
+        console.log(errro);
+        return res.status(500).json({message: "Something went wrong"});
+    }
 };
 
-module.exports = {registerUser};
+const loginUser = async (req, res) => {
+    const {email, password} = req.body;
+    
+    try {
+        let user = await UserModel.findOne({email});
+        if (!user) return res.status(400).json("Invalid Credentials");
+        
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!isValidPassword) return res.status(400).json("Invalid Credentials");
+        
+        const token = createToken(user._id);
+
+        res.status(200).json({message: "Logged in successfully"});
+    } catch (error) {
+        
+    }
+};
+
+module.exports = {registerUser, loginUser};
