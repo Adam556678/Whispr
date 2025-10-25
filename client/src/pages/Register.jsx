3815ab0a4e7a8a576f22157184d115ad08c98405
Adam556678 <mahmudramadan75@yahoo.com>
2025-10-25 21:56:19 +0300

    Implementing Register state managmenet

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,10 +3,10 @@ import {AuthContext} from "../context/AuthContext";
 import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
 
 const Register = () => {
-    const {registerInfo, updateRegisterInfo} = useContext(AuthContext);
+    const {registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading} = useContext(AuthContext);
 
     return <>
-        <Form>
+        <Form onSubmit={registerUser}>
             <Row style={{
                 height: "100vh",
                 justifyContent: "center",
@@ -37,10 +37,14 @@ const Register = () => {
                         }
                         />
                         <Button variant="primary" type="submit">
-                            Register
+                            {isRegisterLoading ? "Creating your account..." : "Register"}
                         </Button>
 
-                        <Alert variant="danger"><p>An error occured</p></Alert>
+                        {
+                            registerError?.error && <Alert variant="danger">
+                                <p>{registerError?.message}</p>
+                            </Alert>
+                        }
                     </Stack>
                 </Col>
             </Row>
