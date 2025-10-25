e95330257dc38006518764208ad2f3a6a3c53c0e
Adam556678 <mahmudramadan75@yahoo.com>
2025-10-25 13:34:49 +0300

    Implement Login and Register page UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,27 @@
 const Login = () => {
-    return ( <>Login</> );
+    return <>
+        <Form>
+            <Row style={{
+                height: "100vh",
+                justifyContent: "center",
+                paddingTop: "10%"
+            }}>
+                <Col xs={6}>
+                    <Stack gap={3}>
+                        <h2>Login</h2>
+
+                        <Form.Control type="email" placeholder="Email"/>
+                        <Form.Control type="Password" placeholder="Password"/>
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+
+                        <Alert variant="danger"><p>An error occured</p></Alert>
+                    </Stack>
+                </Col>
+            </Row>
+        </Form>
+    </>;
 }
  
 export default Login;
