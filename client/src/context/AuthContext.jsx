bd0b91f4566ac1f08d5e328ff04d049395b4f871
Adam556678 <mahmudramadan75@yahoo.com>
2025-10-25 20:41:34 +0300

    Implement Auth context

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,12 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({children}) => {
-    const [user, setUser] = useState({
-        name: "Charles"
-    })
-    return <AuthContext.Provider value={{user}}>
+    const [user, setUser] = useState({});
+    const [registerInfo, setRegisterInfo] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+
+    console.log("registerInfo", registerInfo);
+
+    const updateRegisterInfo = useCallback((info) => {
+        setRegisterInfo(info);
+    }, []);
+
+    return <AuthContext.Provider
+        value={{
+            user,
+            registerInfo,
+            updateRegisterInfo
+            }}
+        >
         {children}
     </AuthContext.Provider>
 };
